ac53a5e5421aa4772a6b0c6869d5b00ad250d705
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-10 16:24:46 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
+import Title from './components/Title';
+import ShowCount from './components/ShowCount';
+import Button from './components/Button';
 
 export default function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const incrementByOne = () => {
+    setCount1(prevState => prevState + 1);
+  };
+
+  const incrementByFive = () => {
+    setCount2(prevState => prevState + 5);
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Title />
+      <ShowCount count={count1} title="Counter 1" />
+      <Button handleClick={incrementByOne}>Increment By One</Button>
+      <hr />
+      <ShowCount count={count2} title="Counter 2" />
+      <Button handleClick={incrementByFive}>Increment By Five</Button>
     </div>
   );
 }
