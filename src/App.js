e105f7d9a90575708f175f0fecfc56a3dfd9115d
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-10 16:48:02 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -16,12 +16,21 @@ export default function App() {
     setCount2(prevState => prevState + 5);
   }, []);
 
+  const isEvenOrOdd = () => {
+    let i = 0;
+    while (i < 10000000000) {
+      i = i + 1;
+    }
+    return count1 % 2 === 0;
+  };
+
   return (
     <div>
       <Title />
       <ShowCount count={count1} title="Counter 1" />
       <Button handleClick={incrementByOne}>Increment By One</Button>
       <hr />
+      <span> {isEvenOrOdd() ? 'Even' : 'Odd'}</span>
       <ShowCount count={count2} title="Counter 2" />
       <Button handleClick={incrementByFive}>Increment By Five</Button>
     </div>
